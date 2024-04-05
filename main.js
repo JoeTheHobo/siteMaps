d3dcd3d547d36d6cc79e1649362592599c35e384
JoeTheHobo <36902814+JoeTheHobo@users.noreply.github.com>
2024-04-04 18:18:47 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,165 @@
+/*
+    Possible Class Types
+    autherizedPeoples - Txt (Name, Number, Position)
+    Keys - (Names, Cuts)
+    Contacts - (Names, Numbers)
+    Images - (images)
+*/
+function tern(clause,els) {
+    if (clause) return clause
+    else return els
+};
 class fileSystem {
-    constructor(obj) {
+    constructor(name,obj={}) {
 
         this.kids = [];
-        this.templateFolder = new folder("Template_Folder",false);
-        this.templateFolder.add(new template({
-            name: "basic",
-        }))
-        this.templateFolderIcon = "img/pinningChart.png";
-        this.templateFolderTitle = "Templates";
-        this.background = obj.background ? obj.background : "rgb(144, 113, 201)";
+        this.display = obj.display;
+        this.pathDisplay = obj.pathDisplay;
+        this.css = {
+            background: tern(obj.cssBackground,"black"),
+            color: tern(obj.cssColor,"white"),
+        }
+        this.cssBackground = 
+        this.goBack = obj.goBack;
+        this.addMenu = obj.addMenu;
+        this.addList = [folder,building,door,template];
+
+        this.path = name + ":";
+
+        let templateFolder = new folder("Templates",{isKillable: false,addList: [template]});
+        templateFolder.add(new template("basic"));
+        this.add(templateFolder);
     }
     add = function(toAdd) {
+        toAdd.display = this.display;
+        toAdd.pathDisplay = this.pathDisplay;
+        toAdd.path = this.path + toAdd.name + '/';
+        toAdd.goBack = this.goBack;
+        toAdd.addMenu = this.addMenu;
+        toAdd.parent = this;
+
         this.kids.push(toAdd);
+    }
+    open = function() {
+        this.updateCSS();
+        
+        let addMenu = this.addMenu;
+        this.goBack.onclick = function() {
+            addMenu.style.display = 'none';
+        }
+
+        
+        buildAddMenu(this.addMenu,this.addList,this);
+        
+
+        render(this,this.display);
+        if (this.pathDisplay) this.pathDisplay.innerHTML = this.path;
+        return this.path;
+    }
+    updateCSS = function() {
+        this.display.style.background = this.css.background;
+        this.display.style.color = this.css.color;
+
     }
 }
 class folder {
-    constructor(name,isKillable = true) {
+    constructor(name,obj = {}) {
         this.name = name;
         this.kids = [];
-        this.isKillable = isKillable;
-        this.iconSRC = "img/folder.png"
+        this.isKillable = obj.isKillable ? obj.isKillable : true;
+        this.iconSRC = "img/fullFolder.png"
+
+        this.display = null; //To Be Set From OS;
+        this.pathDisplay = null; //To Be Set From OS;
+        this.path = null; //To Be set From OS:
+        this.goBack = null;//To Be set From OS:
+        this.parent = obj.parent ? obj.parent : null; //To Be Set From OS;
+        this.addMenu = null; //To Be Set From OS;
+        this.addList = obj.addList ? obj.addList : [folder,building,door,template];
+        this.headers = obj.headers ? obj.headers : [];
+
+
     }
     add = function(toAdd) {
+        toAdd.display = this.display;
+        toAdd.path = this.path + toAdd.name + '/';
+        toAdd.pathDisplay = this.pathDisplay;
+        toAdd.goBack = this.goBack;
+        toAdd.parent = this;
+        toAdd.addMenu = this.addMenu;
+
         this.kids.push(toAdd);
+    }
+    open = function() {
+        
+        let parent = this.parent;
+        let addMenu = this.addMenu;
+        let path = this.path;
+        this.goBack.onclick = function() {
+            forwardPath = path;
+            parent.open();
+            addMenu.style.display = 'none';
+        }
+
+        
+        buildAddMenu(this.addMenu,this.addList,this);
+        
+
+        render(this,this.display);
+        if (this.pathDisplay) this.pathDisplay.innerHTML = getPathName(this.path,this.pathDisplay);
+        return this.path;
     }
 }
 class building {
-    constructor(obj) {
+    constructor(name,obj = {}) {
         this.template = obj.template;
-        this.name = obj.name;
+        this.name = name;
         this.address = obj.address;
         this.doors = [];
         this.iconSRC = "img/church.png"
+        this.kids = [];
+
+        this.display = null; //To Be Set From OS;
+        this.path = null; //To Be set From OS:
+        this.pathDisplay = null; //To Be Set From OS;
+        this.goBack = null;//To Be set From OS:
+        this.parent = obj.parent ? obj.parent : null; //To Be Set From OS;
+        this.addMenu = null; //To Be Set From OS;
+        this.addList = obj.addList ? obj.addList : [folder,door];
+    }
+    add = function(toAdd) {
+        toAdd.display = this.display;
+        toAdd.path = this.path + toAdd.name + '/';
+        toAdd.pathDisplay = this.pathDisplay;
+        toAdd.goBack = this.goBack;
+        toAdd.parent = this;
+        toAdd.addMenu = this.addMenu;
+
+        this.kids.push(toAdd);
+    }
+    open = function() {
+        
+        let parent = this.parent;
+        let addMenu = this.addMenu;
+        this.goBack.onclick = function() {
+            parent.open();
+            addMenu.style.display = 'none';
+        }
+
+        buildAddMenu(this.addMenu,this.addList,this);
+        
+
+        
+        render(this,this.display);
+        if (this.pathDisplay) this.pathDisplay.innerHTML = this.path;
+        return this.path;
     }
 }
 class door {
-    constructor(obj) {
+    constructor(name,obj) {
         this.iconSRC = "img/siteMap.png"
 
+        this.name = name;
         this.floor = obj.floor;
         this.number = obj.number;
         this.quantity = obj.quantity;
@@ -49,12 +171,22 @@ class door {
         this.discription = obj.discription;
         this.keyedTo = obj.keyedTo;
         this.finish = obj.finish;
+
+        this.display = null; //To Be Set From OS;
+        this.path = null; //To Be set From OS:
+        this.pathDisplay = null; //To Be Set From OS;
+        this.goBack = null;//To Be set From OS:
+        this.parent = obj.parent ? obj.parent : null; //To Be Set From OS;
+        this.addMenu = null; //To Be Set From OS;
+    }
+    open = function() {
+        
     }
 }
 class template {
-    constructor(obj) {
+    constructor(name,obj = {}) {
         this.iconSRC = "img/book.png"
-        this.name = obj.name;
+        this.name = name;
         
         this.location = obj.location ? obj.location : false;
         this.type = obj.type ? obj.type : false;
@@ -63,51 +195,245 @@ class template {
         this.discription = obj.discription ? obj.discription : false;
         this.keyedTo = obj.keyedTo ? obj.keyedTo : false;
         this.finish = obj.finish ? obj.finish : false;
+
+        this.display = null; //To Be Set From OS;
+        this.path = null; //To Be set From OS:
+        this.pathDisplay = null; //To Be Set From OS;
+        this.goBack = null;//To Be set From OS:
+        this.parent = obj.parent ? obj.parent : null; //To Be Set From OS;
+        this.addMenu = null; //To Be Set From OS;
+    }
+    open = function() {
+        
     }
 }
 
+function buildAddMenu(addMenuDom,addList,parent) {
+    addMenuDom.innerHTML = "<div class='addItemText'>Add Item</div>";
+    for (let i = 0; i < addList.length; i++) {
+        let div = addMenuDom.create("div");
+        div.className = "option";
+        div.innerHTML = addList[i].name.charAt(0).toUpperCase() + addList[i].name.substring(1,addList[i].name.length);
+        div.Class = addList[i];
+
+        div.onclick = function() {
+            addMenu();
+            parent.add(new div.Class(prompt("Name?")))
+            parent.open();
+        }
+    }
+}
 function render(file,dom) {
-    let folderSize = ((window.innerWidth-80)/4) * fileSize;
-    let folderImageSize = (((window.innerWidth-80)/4)*0.625) * fileSize;
+    let folderSize = ((window.innerWidth-140)/4) * fileSize;
+    let folderImageSize = (((window.innerWidth-140)/4)*1) * fileSize;
     let ids = 0;
 
     dom.innerHTML = '';
+    
+    if (file.headers) {
+        let headers = `<div class="headers">`;
+        for (let i  = 0; i < file.headers.length; i++) {
+            headers += `<div class='header'>${file.headers[i]}</div>`;
+        }
+        headers += "</div>";
+        dom.innerHTML += headers;
+    }
+
     if (file.templateFolder) {
-        ids++;
         dom.innerHTML += createFileDom(file.templateFolderIcon,file.templateFolderTitle,folderSize,folderImageSize,ids);
+        ids++;
         
     }
-    console.log(file);
     for (let i = 0; i < file.kids.length; i++) {
-        ids++;
         dom.innerHTML += createFileDom(file.kids[i].iconSRC,file.kids[i].name,folderSize,folderImageSize,ids);
+        ids++;
         
+    }
+    dom.innerHTML += getDOMAddButton();
+    
+    $('addButton').onclick = function() {
+        addMenu(this);
     }
     for (let i = 0; i < ids; i++) {
         $('folderIMG' + i).file = file.kids[i];
     }
 }
+var mylatesttap;
+var oldEle;
+function doubletap(element) {
+
+   var now = new Date().getTime();
+   var timesince = now - mylatesttap;
+   if((timesince < 300) && (timesince > 0)){
+    if (element == oldEle)
+        openFolder(element);
+
+   }else{
+            // too much time to be a doubletap
+         }
+
+   mylatesttap = new Date().getTime();
+   oldEle = element;
+
+}
 function createFileDom(src,name,width1,width2,id) {
     return `
         <div style="max-width: ${width1}px;width:${width1}px" class="folder">
-            <img id="folderIMG${id}" onclick="openFolder(this)" style="width:${width2}px" class="folderImage" src=${src}>
-            <div style="width:${width1}px" class="folderTitle">${name}</div>
+            <img id="folderIMG${id}" onclick="doubletap(this)" style="width:${width2}px" class="folderImage" src=${src}>
+            <div style="font-size: ${width1*.2}px;width:${width1}px" class="folderTitle">${name}</div>
         </div>
     `;
 }
+function getTextWidth(txt) { 
+    text = document.createElement("span"); 
+    document.body.appendChild(text); 
+    
+    text.style.font = "times new roman"; 
+    text.style.fontSize = 16 + "px"; 
+    text.style.height = 'auto'; 
+    text.style.width = 'auto'; 
+    text.style.position = 'absolute'; 
+    text.style.whiteSpace = 'no-wrap'; 
+    text.innerHTML = txt; 
+    
+    width = Math.ceil(text.clientWidth); 
+    document.body.removeChild(text); 
+    return width;
+} 
+function getPathName(pathString,inputDom) {
+    //Get Arrays Of Path
+    let pathArray = [];
+    let width = inputDom.clientWidth- 50;
+
+    let string = "";
+    for (let i = 0; i < pathString.length; i++) {
+        if (pathString.charAt(i) == ":" || pathString.charAt(i) == "/") {
+            pathArray.push(string);
+            string = "";
+            continue;
+        }
+        string += pathString.charAt(i);
+    }
+
+    let tooLong = true;
+    while (tooLong) {
+        //Test If Short Enough
+        let returnString = "";
+        for (let i = 0; i < pathArray.length; i++) {
+            returnString += pathArray[i] + (i == 0 ? ":" : '/');
+        }
+        let textWidth = getTextWidth(returnString)
+        if (textWidth < width) {
+            return returnString;
+        }
+
+
+        let madeChange = false;
+        //Shorten Any String
+        //Find Largest String
+        let largestStringI = false;
+        let largestNum = 0;
+        for (let i = 0; i < pathArray.length; i++) {
+            if (pathArray[i].length > largestNum) {
+                largestNum = pathArray[i].length;
+                largestStringI = i;
+            }
+        }
+
+        testingString: if (largestStringI) {
+            //Length Has To be bigger than 4
+            if (largestNum < 5) break testingString;
+
+            let isShortened = false;
+            if (pathArray[largestStringI].substring(pathArray[largestStringI].length-3,pathArray[largestStringI].length) == "...") {
+                isShortened = true;
+            }
+
+            pathArray[largestStringI] = pathArray[largestStringI].substring(0,pathArray[largestStringI].length-4) + "...";
+            madeChange = true;
+
+        }
+
+        //Handel If No Changes Are Possible
+        if (!madeChange) {
+            tooLong = false;
+        }
+    }
+    
+}
+function getDOMAddButton() {
+    return `<div id="addButton" class="addButton">+</div>`;
+}
+function addMenu(element) {
+    element = $('addButton');
+    if ($('.addMenu').style.display == "none" || $('.addMenu').style.display == "") {
+        $('.addMenu').style.display = "block";
+        var rect = element.getBoundingClientRect();
+
+        let left = (rect.left - 65);
+        let right = false;
+        let top = (rect.bottom + 10);
+        let bottom = rect.bottom > window.innerHeight/2 ? (window.innerHeight-rect.bottom)+rect.height + 5 : false;
+
+        if (left < 0) left = 5;
+        if ((left + 214) > window.innerWidth) {
+            right = 50;
+        }
+
+        if (bottom) {
+            $('.addMenu').style.bottom = bottom +'px';
+            $('.addMenu').style.top = "auto";
+        } else {
+            $('.addMenu').style.top = top +'px';
+            $('.addMenu').style.bottom = 'auto';
+        }
+        if (right != false) {
+            $('.addMenu').style.right = right +'px';
+            $('.addMenu').style.left = 'auto';
+        } else {
+            $('.addMenu').style.left = left +'px';
+            $('.addMenu').style.right = 'auto';
+        } 
+    } else {
+        $('.addMenu').style.display = "none";
+    }
+};
 
 function openFolder(element) {
-    render(element.file,$('fileSystem'));
+    element.file.open();
+}
+$('goForward').addEventListener("click",function() {
+    goInDirectory(os,forwardPath);
+})
+function goInDirectory(dir,path) {
+    let paths = path.split('/');
+    let splitColon = paths[0].split(":");
+    paths.shift();
+    let newPath = splitColon.concat(paths);;
+    for (let i = 1; i < newPath.length-1; i++) {
+        findKidInDir: for (let j = 0; j < dir.kids.length; j++) {
+            if (dir.kids[j].name == newPath[i]) {
+                dir = dir.kids[j];
+                break findKidInDir;
+            }
+        }
+    }
+    dir.open();
 }
 
-var fileSize = 1;
-var operatingSystem = false;
+var fileSize = 1.2;
+let filePath = "";
+let forwardPath = false;
 
-let os = new fileSystem({});
-operatingSystem = os;
-let og = new folder("Ey");
-og.add(new building("hey"));
-os.add(og);
-os.add(new building("Testinggggggggggg"));
-os.add(new door("Testinggggggggggg"));
-render(os,$("fileSystem"))
+let os = new fileSystem("J",{
+    display: $("fileSystem"),
+    pathDisplay: $("path"),
+    goBack: $('goBack'),
+    addMenu: $(".addMenu"),
+});
+
+os.add(new folder("File Name 1",{
+    headers: ["Santaquin City Police Department","346 N 100 E, Santaquin"],
+}))
+
+os.open();
